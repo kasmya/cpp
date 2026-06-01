4a2b485d4b05f922e7dd8c3ea222bbdcc487cf0b
kasmya bhatia <109264892+kasmya@users.noreply.github.com>
2026-06-01 12:45:15 +0530

    Update arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -39,3 +39,16 @@ int main(){
     return 0;
 }
 
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[5];
+    for (int i=0; i<5; i++){
+        cin>>arr[i];
+    }
+    for (int i=0; i<5; i++){
+        cout<<arr[i]<<" ";
+    }
+return 0;
+}
+
