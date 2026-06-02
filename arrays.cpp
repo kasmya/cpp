dcca06ef79f0248ad01088769685e8fe9b352519
kasmya bhatia <109264892+kasmya@users.noreply.github.com>
2026-06-02 12:39:44 +0530

    Add main function to display original array

--- arrays.cpp
+++ arrays.cpp
@@ -77,3 +77,13 @@ int main(){
     cout<<endl;
     return 0;
 }
+// b) at any index 
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[6] = {1,2,3,4,5};
+    cout<<"original array:";
+    for(int i=0; i<5; i++){
+        cout<<arr[i]<<" ";
+    }
+cout<<endl;
