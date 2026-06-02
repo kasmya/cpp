6996c59a6204d2eb31610e4b91e88d6fc1ed4593
kasmya bhatia <109264892+kasmya@users.noreply.github.com>
2026-06-02 13:38:35 +0530

    Add array deletion functionality
    Implement deletion of the first element in an array.

--- arrays.cpp
+++ arrays.cpp
@@ -115,3 +115,26 @@ int main(){
     cout<<endl;
     return 0;
 }
+// 3. deletion 
+// a) at starting 
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[5]={1,2,3,4,5};
+    int n=5; 
+    cout<<"original array:";
+    for (int i=0; i<5; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    for (int i=0; i<n-1; i++){
+        arr[i] = arr[i+1];
+    }
+    n--;
+    for (int i=0; i>n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
+    cout<<endl;
