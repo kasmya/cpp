5c3ac466723f4a0f1b3811fcd2b2d136ea2a1904
kasmya bhatia <109264892+kasmya@users.noreply.github.com>
2026-06-07 20:23:28 +0530

    Implement 2D array printing and vector usage
    Added code to print elements of a 2D array and introduced usage of vectors.

--- 2darrays.cpp
+++ 2darrays.cpp
@@ -24,5 +24,40 @@ int main(){
   arr[2][0]=5;
   arr[2][1]=6;
   int brr[2][3]={{1,2,3},{4,5,6}};
-  return 0;
+  for(int i=0; i<2; i++){
+    for (int j=0; j<3; j++){
+      cout<<brr[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+}
+// limitations of arrays: 
+// 1. fixed size 
+// 2. stack overflow 
+// 3. no built in functions available in it 
+// 4. cannot resize 
+
+// vector/ dynamic array
+// 1. automatically resizable 
+// 2. easy to return and pass around 
+// 3. frequently used at coding platforms like leetcode 
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+  vector<int> arr;
+  vector<int> arr1(6);
+  vector<int> arr2(3,9);
+  cout<<"taking input in the vector"<<endl;
+  for (int i=0; i<6; i++){
+    cin>>arr1[i]<<" ";
+  }
+  cout<<"printing the vector"<<endl;
+  for (int i=0; i<6; i++){
+    cout<<arr1[i]<<" ";
+  }
+  // 2d array vector declaration
+  vector<vector<int>> mat(3,vector<int>(2));
+  vector <int> brr;
+  brr.push_back(6); //insertion
 }
