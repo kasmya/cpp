1bcc4c016a19fa86d1c69f02dc88655f60d953c3
kasmya bhatia <109264892+kasmya@users.noreply.github.com>
2026-07-05 15:27:17 +0530

    Enhance comments and add bubble sort pseudo code
    Updated comments to clarify sorting process and added pseudo code for bubble sort.

--- sorting.cpp
+++ sorting.cpp
@@ -12,7 +12,7 @@
 // eg array: 5 4 3 2 1 
 
 // pass 1: 5 4 3 2 1 
-// n - pass = 5 - 1 = 4 steps 
+// n - pass = 5 - 1 = 4 steps - hence no need to check last element (5th)
 // if i > i+1 then swap element 
 // 4 5 3 2 1
 // 4 3 5 2 1
@@ -32,4 +32,43 @@
 
 // pass 4: 2 1 3 4 5
 // 5 - 4 = 1
-// 1 2 3 4 5
+// 1 2 3 4 5 sorted array 
+
+// psuedo code
+// for (pass =1; pass<= n-1; pass++)
+// {
+// for (int i = 0; i<=n-1-pass; i++)
+// {
+// if (arr[i] > arr[i+1])
+// {
+// swap(arr[i], arr[i+1]) 
+// }
+// }
+// }
+
+#include <iostream>
+using namespace std;
+int main(){
+  int arr[] = {1,2,3,4,5,6};
+  int size = sizeof(arr)/sizeof(arr[0]);
+  cout<< "printing original array:"<< endl;
+  for (int i = 0; i<size; i++){
+    cout<<arr[i]<< " ";
+  }
+  cout<< endl;
+  
+  for (int pass = 1; pass<size; pass++){
+    for (int i = 0; i<size-pass; i++){
+      if (arr[i] > arr[i+1]){
+        swap(arr[i], arr[i+1]);
+      }
+    }
+  }
+  
+  cout<< "printing after sorting:"<<endl;
+  for (int i = 0; i<size; i++){
+    cout<<arr[i]<< " ";
+  }
+  cout<< endl;
+  return 0;
+}
