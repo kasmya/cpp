fd7e4f5e3c914537bbeeeb880f961cf0c817a67b
kasmya bhatia <109264892+kasmya@users.noreply.github.com>
2026-07-05 14:18:35 +0530

    Add comments to sorting.cpp
    Add comments explaining the purpose of sorting.

---
+++ sorting.cpp
@@ -0,0 +1,4 @@
+// SORTING 
+// - Faster searching like binary search
+// - Better organisation for real life tasks like contact lists or leaderboards
+// - Foundation for many algos like solving duplicate problems, range queries 
