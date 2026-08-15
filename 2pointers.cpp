b100ea7fd487a0bcc3362f50fb817cce5c8a2aa3
kasmya bhatia <109264892+kasmya@users.noreply.github.com>
2026-08-15 11:24:14 +0530

    Add sortedSquares function for sorted array squares
    Implement sortedSquares function to return squares of sorted array.

--- 2pointers.cpp
+++ 2pointers.cpp
@@ -196,3 +196,37 @@ public:
         return result; // intersection with duplicates
     }
 };
+// PROBLEM 977: SQUARES OF SORTED ARRAY
+/*Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+ 
+
+Example 1:
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+Example 2:
+
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+ */
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+        int i = 0, j = n - 1, k = n - 1;
+        while (i <= j) {
+            if (abs(nums[i]) > abs(nums[j])) {
+                result[k--] = nums[i] * nums[i];
+                i++;
+            } else {
+                result[k--] = nums[j] * nums[j];
+                j--;
+            }
+        }
+        return result;
+    }
+};
