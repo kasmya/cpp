90569908f8ab37f6bbebc1b15e38968bfbbe991e
kasmya bhatia <109264892+kasmya@users.noreply.github.com>
2026-08-18 00:44:32 +0530

    Optimize reverseString with two-pointer technique
    Refactor reverseString method to use two pointers for in-place reversal.

--- ques.cpp
+++ ques.cpp
@@ -15,6 +15,7 @@ Example 2:
 Input: s = ["H","a","n","n","a","h"]
 Output: ["h","a","n","n","a","H"]
 */
+//O(N) TIME COMPLEXITY
 class Solution {
 public:
     void reverseString(vector<char>& s) {
@@ -24,5 +25,18 @@ public:
             ans.push_back(s[i]);
         }
         s=ans;
+    }
+};
+//O(1) TIME COMPLEXITY - using 2 pointers
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n=s.size();
+        int i =0 , j =n-1;
+        while (i<j){
+         swap(s[i],s[j]);
+         i++;
+         j--;
+        }
     }
 };
