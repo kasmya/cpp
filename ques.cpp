bf0b6a83953ba4cd0643e4fffc5c13857c78502e
kasmya bhatia <109264892+kasmya@users.noreply.github.com>
2026-08-17 23:55:06 +0530

    Add function to reverse a string
    Implemented a function to reverse a string in place.

--- ques.cpp
+++ ques.cpp
@@ -15,3 +15,14 @@ Example 2:
 Input: s = ["H","a","n","n","a","h"]
 Output: ["h","a","n","n","a","H"]
 */
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n=s.size();
+        vector<char> ans;
+        for(int i=n-1; i>=0; i--){
+            ans.push_back(s[i]);
+        }
+        s=ans;
+    }
+};
